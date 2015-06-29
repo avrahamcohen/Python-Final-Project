259816724958b63831114f752410fd1d16713d12
Avraham <avrahamcohen.ac@gmail.com>
2015-06-29 20:45:08 +0300

    Final Project Files

---
+++ scores.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+# Full Name: Avraham Cohen ID:038025797
+# Final Project: Udacity Programming Foundations with Python
+# Excel Sheet - scores.xlsx
+
+# This class represent one row from the Excel sheet
+class score_class(object):
+    def __init__(self, date, snatch, clean, jerk, dead_lift, back_squat, front_squat, over_head_squat, bench_press):
+        self.date = date
+        self.snatch = snatch
+        self.clean = clean
+        self.jerk = jerk
+        self.dead_lift = dead_lift
+        self.back_squat = back_squat
+        self.front_squat = front_squat
+        self.over_head_squat = over_head_squat
+        self.bench_press = bench_press
+
+    @staticmethod
+    def getItem(item, items):
+	    values = []
+
+	    if (item == 'date'):
+	      for item in items:
+	        values = values + [format(item.date)]
+	      return values
+
+	    if (item == 'snatch'):
+	      for item in items:
+	        values = values + [format(item.snatch)]
+	      return values
+
+	    if (item == 'clean'):
+	      for item in items:
+	        values = values + [format(item.clean)]
+	      return values
+
+	    if (item == 'jerk'):
+	      for item in items:
+	        values = values + [format(item.jerk)]
+	      return values
+
+	    if (item == 'dead_lift'):
+	      for item in items:
+	        values = values + [format(item.dead_lift)]
+	      return values
+
+	    if (item == 'back_squat'):
+	      for item in items:
+	        values = values + [format(item.back_squat)]
+	      return values
+
+	    if (item == 'front_squat'):
+	      for item in items:
+	        values = values + [format(item.front_squat)]
+	      return values
+
+	    if (item == 'over_head_squat'):
+	      for item in items:
+	        values = values + [format(item.over_head_squat)]
+	      return values
+
+	    if (item == 'bench_press'):
+	      for item in items:
+	        values = values + [format(item.bench_press)]
+	      return values
+
+    @staticmethod
+    def getElementNames():
+      return ['Snatch', 'Clean', 'Jerk', 'Dead Lift', 'Back Squat', 'Front Squat', 'Over Head Squat', 'Bench Press']
+
+    @staticmethod
+    def getValueStructure():
+    	return [[],[],[],[],[],[],[],[],[]]
+
+    @staticmethod
+    def getColors():
+    	return ['blue','green','red','orange','magenta','yellow','black','gray']
